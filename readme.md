d5e9822bfd0bf7630f0045e55a1ec36cb713ca63
Dhanya-krishnan2 <dhanyakunhi@gmail.com>
2020-05-28 22:21:55 +0800

    first commit

---
+++ readme.md
@@ -0,0 +1,9 @@
+This is a password Generator program by using javascript. The minimum characters we are using to create a password is 8 according to users requirement and maximum is 128 character.
+I made a program which satisfy the customer requirement by adding uppercase, lowercase(that is by default), special charecters and numbers. 
+To make very clean code I used ASCII code for my program.
+I declared constant values to call function when needed.
+The other great advantage of javascript I used is the mathfunction, that math.floor and math.random. Math.random gets the random value from an array and math.floor makes that random value in to an integer.
+User can input the amount of elements they need in their password from 8 to 128 and according their choice they may output with the strong password.
+I used event handling function to make it more interactive.
+My screenshots of the output are given in a separate folder.
+I used input tags to make slider output which helps the users to slide the bar to assign the amount of elements they need in their password.
---
+++ script.js
@@ -0,0 +1,61 @@
+//declaring constant for all the available keywords to use it in a function//
+const characterAmountRange = document.getElementById('characterAmountRange')
+const characterAmountNumber = document.getElementById('characterAmountNumber')
+const includeUppercaseElement = document.getElementById('includeUppercase')
+const includeNumbersElement = document.getElementById('includeNumbers')
+const includeSymbolsElement = document.getElementById('includeSymbols')
+const form = document.getElementById('passwordGeneratorForm')
+const passwordDisplay = document.getElementById('passwordDisplay')
+//using ASCII standards to use uppercase, lowercase, number, and special charactors and concatinating the array to add many range to add special charectors//
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
+const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
+  arrayFromLowToHigh(58, 64)
+).concat(
+  arrayFromLowToHigh(91, 96)
+).concat(
+  arrayFromLowToHigh(123, 126)
+)
+
+characterAmountNumber.addEventListener('input', syncCharacterAmount)
+characterAmountRange.addEventListener('input', syncCharacterAmount)
+//adding the Eventlistners to form and preventing the default value which occur by calling the below function//
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  const characterAmount = characterAmountNumber.value
+  const includeUppercase = includeUppercaseElement.checked
+  const includeNumbers = includeNumbersElement.checked
+  const includeSymbols = includeSymbolsElement.checked
+  const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
+  passwordDisplay.innerText = password
+})
+//this is the function to generate the function//
+function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
+  let charCodes = LOWERCASE_CHAR_CODES
+  if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
+  if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
+  if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
+  //declaring an empty array  and then assigning the value to empty array by using 'join'//
+  const passwordCharacters = []
+  for (let i = 0; i < characterAmount; i++) {
+    const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+    passwordCharacters.push(String.fromCharCode(characterCode))
+  }
+  return passwordCharacters.join('')
+}
+// we are using two math function called math floor and math random //
+function arrayFromLowToHigh(low, high) {
+  const array = []
+  for (let i = low; i <= high; i++) {
+    array.push(i)
+  }
+  return array
+}
+
+function syncCharacterAmount(e) {
+  const value = e.target.value
+  characterAmountNumber.value = value
+  characterAmountRange.value = value
+}
+
